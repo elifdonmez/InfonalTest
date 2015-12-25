141146caf3b3ef5456c14d83ea21a7f53c811529
Berker Bilgi <berkerbilgi@outlook.com>
2015-12-25 23:14:50 +0200

    Added log4j and maven

---
+++ src/Driver.java
@@ -0,0 +1,15 @@
+/**
+ * Elif Donmez
+ */
+
+import org.apache.log4j.Logger;
+
+public class Driver
+{
+    private static final Logger logger = Logger.getLogger();
+    
+    public static void main(String[] args)
+    {
+        logger.fatal("fatal log message");
+    }
+}
